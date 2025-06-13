5c59c84c4d9c29332fb6c2f5d35a1d740204f202
Vikash Kumar <rkvikashraj@gmail.com>
2025-06-13 19:58:12 +0530

    S3 bucket complete files CRUD completed

---
+++ S3.Demo.API/Controllers/BucketsController.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.S3;
+using Amazon.S3.Util;
+using Microsoft.AspNetCore.Mvc;
+
+namespace S3.Demo.API.Controllers
+{
+    public class BucketsController : Controller
+    {
+        private readonly IAmazonS3 _s3Client;
+        public BucketsController(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBucketAsync(string bucketName)
+        {
+            // Use AmazonS3Util to check if the bucket exists
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (bucketExists) return BadRequest($"Bucket {bucketName} already exists.");
+            await _s3Client.PutBucketAsync(bucketName);
+            return Ok($"Bucket {bucketName} created.");
+        }
+        [HttpGet("list")]
+        public async Task<IActionResult> ListBucketsAsync()
+        {
+            var response = await _s3Client.ListBucketsAsync();
+            return Ok(response.Buckets.Select(b => b.BucketName));
+        }
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteBucketAsync(string bucketName)
+        {
+            // Use AmazonS3Util to check if the bucket exists
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+
+            // Delete the bucket
+            await _s3Client.DeleteBucketAsync(bucketName);
+            return Ok($"Bucket {bucketName} deleted.");
+        }
+    }
+}
---
+++ S3.Demo.API/Controllers/FilesController.cs
@@ -0,0 +1,77 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Util;
+using Microsoft.AspNetCore.Mvc;
+using S3.Demo.API.Models;
+
+namespace S3.Demo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilesController : Controller
+    {
+        private readonly IAmazonS3 _s3Client;
+        public FilesController(IAmazonS3 amazonS3)
+        {
+            _s3Client = amazonS3;
+        }
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFileAsync(IFormFile file, string bucketName, string? prefix)
+        {
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new PutObjectRequest()
+            {
+                BucketName = bucketName,
+                Key = string.IsNullOrEmpty(prefix) ? file.FileName : $"{prefix?.TrimEnd('/')}/{file.FileName}",
+                InputStream = file.OpenReadStream()
+            };
+            request.Metadata.Add("Content-Type", file.ContentType);
+            await _s3Client.PutObjectAsync(request);
+            return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
+        }
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllFilesAsync(string bucketName, string? prefix)
+        {
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new ListObjectsV2Request()
+            {
+                BucketName = bucketName,
+                Prefix = prefix
+            };
+            var result = await _s3Client.ListObjectsV2Async(request);
+            var s3Objects = result.S3Objects.Select(s =>
+            {
+                var urlRequest = new GetPreSignedUrlRequest()
+                {
+                    BucketName = bucketName,
+                    Key = s.Key,
+                    Expires = DateTime.UtcNow.AddMinutes(1)
+                };
+                return new S3ObjectDto()
+                {
+                    Name = s.Key.ToString(),
+                    PresignedUrl = _s3Client.GetPreSignedURL(urlRequest),
+                };
+            });
+            return Ok(s3Objects);
+        }
+        [HttpGet("preview")]
+        public async Task<IActionResult> GetFileByKeyAsync(string bucketName, string key)
+        {
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var s3Object = await _s3Client.GetObjectAsync(bucketName, key);
+            return File(s3Object.ResponseStream, s3Object.Headers.ContentType);
+        }
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteFileAsync(string bucketName, string key)
+        {
+            var bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist");
+            await _s3Client.DeleteObjectAsync(bucketName, key);
+            return NoContent();
+        }
+    }
+}
